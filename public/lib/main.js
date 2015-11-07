67f0af4f1a4e08c8ac1a957a0b9c64d2bce14221
Jonas Hermsmeier <jhermsmeier@gmail.com>
2015-11-07 12:36:04 +0100

    Add stats

--- public/lib/main.js
+++ public/lib/main.js
@@ -5,6 +5,10 @@ var FAR = 1e27
 var NEAR = 0.1
 var FOV = 70
 
+var fps = new Stats()
+var ms = new Stats()
+var mb = new Stats()
+
 var renderer = new THREE.WebGLRenderer({})
 // renderer.setClearColor(new THREE.Color('lightblue'), 1)
 
@@ -31,12 +35,48 @@ function resize() {
 
 function render() {
 
+  if( DEBUG ) {
+    fps.begin()
+    ms.begin()
+    mb.begin()
+  }
+
   // Time delta for camera controls
   var delta = clock.getDelta()
 
-  window.requestAnimationFrame( render )
   renderer.render( scene, camera )
   // controls.update( delta )
+
+  if( DEBUG ) {
+    fps.end()
+    ms.end()
+    mb.end()
+  }
+
+  window.requestAnimationFrame( render )
+
+}
+
+function addStats() {
+
+  fps.setMode( 0 )
+  fps.domElement.style.position = 'absolute'
+  fps.domElement.style.right = '0px'
+  fps.domElement.style.bottom = '0px'
+
+  ms.setMode( 1 )
+  ms.domElement.style.position = 'absolute'
+  ms.domElement.style.right = '80px'
+  ms.domElement.style.bottom = '0px'
+
+  mb.setMode( 2 )
+  mb.domElement.style.position = 'absolute'
+  mb.domElement.style.right = '160px'
+  mb.domElement.style.bottom = '0px'
+
+  document.body.appendChild( fps.domElement )
+  document.body.appendChild( ms.domElement )
+  document.body.appendChild( mb.domElement )
 
 }
 
@@ -45,6 +85,10 @@ window.addEventListener( 'DOMContentLoaded', function() {
 
   document.querySelector( 'main' )
     .appendChild( renderer.domElement )
+
+  if( DEBUG ) {
+    addStats()
+  }
 
   setupScene()
   resize()
