389ca6a1a1a48463e17d6af2071bad5dc3c5702e
simonhfrost <simonhfrost@gmail.com>
2015-11-07 02:37:48 +0100

    Hook spaceship up

--- public/lib/spaceship.js
+++ public/lib/spaceship.js
@@ -1,7 +1,7 @@
 var loader = new THREE.JSONLoader()
 
 loader.load('models/spaceship.json', function (geometry) {
-  var material = new THREE.MeshLambertMaterial({
+  var material = new THREE.MeshPhongMaterial({
     color: '#ed8989',
     shading: THREE.FlatShading,
     colorAmbient: [0.5, 0.5, 0.5],
@@ -13,6 +13,12 @@ loader.load('models/spaceship.json', function (geometry) {
     geometry,
     material
   )
+
+  spaceShip.scale.x = 10
+  spaceShip.scale.y = 10
+  spaceShip.scale.z = 10
+
+  spaceShip.position.z = 250
 
   scene.add(spaceShip)
 })
