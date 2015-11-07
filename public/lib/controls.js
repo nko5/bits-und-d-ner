3c25c34a711f7268508fda58a2c2eb53ff5a66a2
simonhfrost <simonhfrost@gmail.com>
2015-11-07 14:36:42 +0100

    Use local rotation

--- public/lib/controls.js
+++ public/lib/controls.js
@@ -1,29 +1,29 @@
 document.addEventListener("keydown", function( event ) {
   // PITCH
   if ( event.keyCode === 87 ) { // W
-    spaceship.rotation.x -= 0.1
+    spaceship.rotateX( 0.1 )
   }
 
   if ( event.keyCode === 83 ) { // S
-    spaceship.rotation.x += 0.1
+    spaceship.rotateX( -0.1 )
   }
 
   // YAW
   if ( event.keyCode === 68 ) { // D
-    spaceship.rotation.y -= 0.1
+    spaceship.rotateY( -0.1 )
   }
 
   if ( event.keyCode === 65 ) { // A
-    spaceship.rotation.y += 0.1
+    spaceship.rotateY( 0.1 )
   }
 
   // ROLL
   if ( event.keyCode === 69 ) { // E
-    spaceship.rotation.z -= 0.1
+    spaceship.rotateZ( 0.1 )
   }
 
   if ( event.keyCode === 81 ) { // Q
-    spaceship.rotation.z += 0.1
+    spaceship.rotateZ( -0.1 )
   }
 
 });
