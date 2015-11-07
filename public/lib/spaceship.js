6d945ca16da34a4f805fbcaa0bfee369257a4c5d
simonhfrost <simonhfrost@gmail.com>
2015-11-07 03:01:00 +0100

    Remove unused properties

--- public/lib/spaceship.js
+++ public/lib/spaceship.js
@@ -3,10 +3,7 @@ var loader = new THREE.JSONLoader()
 loader.load('models/spaceship.json', function (geometry) {
   var material = new THREE.MeshPhongMaterial({
     color: '#ed8989',
-    shading: THREE.FlatShading,
-    colorAmbient: [0.5, 0.5, 0.5],
-    colorDiffuse: [0.5, 0.5, 0.5],
-    colorSpecular: [0.9, 0.9, 0.9]
+    shading: THREE.FlatShading
   })
 
   spaceShip = new THREE.Mesh(
