bc427a1a347971c22443038c5e9941f1a7e31347
Jonas Hermsmeier <jhermsmeier@gmail.com>
2015-11-07 01:05:03 +0100

    Initial cleanup

--- app.js
+++ app.js
@@ -1,14 +1,12 @@
-var express = require('express');
-var app = express();
+var express = require( 'express' )
+var app = express()
 
 //Create a static file server
-app.configure(function() {
-  app.use(express.static(__dirname + '/public'));
-});
+app.configure( function() {
+  app.use( express.static( __dirname + '/public' ) )
+})
 
-//Get the dummy data
-require('./server/ddata.js');
+var port = 8080
 
-var port = 8080;
-app.listen(port);
-console.log('Express server started on port %s', port);
+app.listen( port )
+console.log( 'Express server started on port %s', port )
