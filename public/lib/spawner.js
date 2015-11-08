6f786e186f070eaf377833da2d07535240fd1f2e
simonhfrost <simonhfrost@gmail.com>
2015-11-08 19:52:45 +0100

    Add asteroids

--- public/lib/spawner.js
+++ public/lib/spawner.js
@@ -1,18 +1,19 @@
 var interval = 1000
+var ARENA_SIZE = 100
 
 var availableModels = []
 
-// loadModel( 'asteroid', new THREE.Vector3(0,0,25), 0.1, '#AAA', availableModels ),
 loadModel( 'tire', new THREE.Vector3(0,0,35), 0.75, '#EDED89', availableModels ),
 loadModel( 'bin', new THREE.Vector3(0,0,40), 1, '#ED89ED', availableModels ),
 loadModel( 'bottle', new THREE.Vector3(0,0,45), 0.5, '#EDB589', availableModels )
 loadModel( 'tv', new THREE.Vector3(0,0,30), 2, '#8989ED', availableModels )
 
+// Spawn rubbish
 setInterval(function() {
   var position = new THREE.Vector3(
-    Math.random() * 50 - 25,
-    Math.random() * 50 - 25,
-    Math.random() * 50 - 25
+    Math.random() * ARENA_SIZE - ARENA_SIZE / 2,
+    Math.random() * ARENA_SIZE - ARENA_SIZE / 2,
+    Math.random() * ARENA_SIZE - ARENA_SIZE / 2
   )
 
   var randomIndex = Math.floor(Math.random() * availableModels.length)
@@ -34,3 +35,36 @@ setInterval(function() {
   scene.add( model )
 
 }, 1000)
+
+
+// Spawn asteroids
+
+var asteroids = []
+loadModel( 'asteroid', new THREE.Vector3(0,0,0), 0.2, '#B5B5B5', asteroids )
+
+setInterval(function() {
+  var model = asteroids[0].clone()
+
+  model.position.x = -(ARENA_SIZE/2) + (ARENA_SIZE * Math.random())
+  model.position.y = -(ARENA_SIZE/2) + (ARENA_SIZE * Math.random())
+  model.position.z = -(ARENA_SIZE/2)
+
+  setInterval(function() {
+    model.position.z += 0.1
+    if (model.position.z > ARENA_SIZE/2) {
+      scene.remove(model)
+    }
+  }, 20)
+
+  var rotationX = Math.random()/10
+  var rotationY = Math.random()/10
+  var rotationZ = Math.random()/10
+
+  setInterval( function(){
+    model.rotation.x += rotationX
+    model.rotation.y += rotationY
+    model.rotation.z += rotationZ
+  }, 20 )
+
+  scene.add( model )
+}, interval)
